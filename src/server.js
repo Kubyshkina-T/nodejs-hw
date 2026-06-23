7af838621d12179cd41bb075d8a116349f9688c2
Tetiana Kubyshkina <tanyasha_@ukr.net>
2026-06-23 21:16:25 +0200

    upload mail and img branch

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import express from "express";
 import cors from "cors";
 import { errors } from 'celebrate';
-
 import { connectMongoDB } from "./db/connectMongoDB.js";
 import { logger } from "./middleware/logger.js";
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
@@ -10,6 +9,7 @@ import { errorHandler } from "./middleware/errorHandler.js";
 import notesRoutes from "./routes/notesRoutes.js";
 import authRoutes from './routes/authRoutes.js';
 import cookieParser from "cookie-parser";
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(authRoutes);
 app.use(notesRoutes);
-
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
